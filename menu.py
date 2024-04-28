584198efe16c622df95c7730c56f6e97f17bde35
Andres <andresalejandro2001@gmail.com>
2024-04-27 20:18:55 -0400

    Actividad menu actualizada

--- menu.py
+++ menu.py
@@ -13,6 +13,7 @@ menu = True
 while menu: 
     print(" 1. INICIAR SESION\n 2. REGISTRAR USUARIO\n 3. SALIR\n")
     opcion = int(input("Ingrese una opcion\n"))
+    os.system("cls")
     if opcion == 1:
         print ("1.INICIAR SESION")
         if (usuario1 == None and contrasena1 == None) and (usuario2 == None and contrasena2 == None) and (usuario3 == None and contrasena3 == None):
@@ -25,18 +26,22 @@ while menu:
             contr = input("Ingrese contraseña\n")
             if (usu == usuario1 and contr == contrasena1) or (usu == usuario2 and contr == contrasena2) or (usu == usuario3 and contr == contrasena3):
              print("Sesion Iniciada")
-             
+             os.system("cls")
              while True:
                   print("1.Realizar llamada")
                   print("2.Enviar correo electronico")
                   print("3.Cerrar cesion")
                   opc2 = int(input("Ingrese una opcion\n"))
+                  os.system("cls")
                   if opc2 == 1:
                       print("1.Realizar llamada")
-                      numero = int(input("Ingrese un numero de celular de 9 digitos empezando con 9\n"))
+                      numero = (input("Ingrese un numero de celular de 9 digitos empezando con 9\n"))
                       if len(numero) == 9 and numero.startswith('9'):
                          print(f"LLAMANDO AL NUMERO {numero}")
-                         time.sleep(2)
+                         time.sleep(1)
+                         print("Llamada finalizada")
+                         time.sleep(1)
+                         os.system("cls")
                       else:
                          print("Numero ingresado invalido")
                          time.sleep(2)
@@ -46,14 +51,17 @@ while menu:
                          correo = input("Ingrese un correo electronico\n")
                          while not ("@") in correo:
                              print("CORREO INVALIDO, PORFAVOR INGRESE OTRO")
+                             os.system("cls")
                              break
                          else:
                              print("CORREO INGRESA CORRECTAMENTE")
                              mensaje = input("Ingrese el mensaje que desea enviar:\n")
                              print("Mensaje enviado")
                              time.sleep(1)
+                             os.system("cls")
                   elif opc2 == 3:
                       print("CESION FINALIZADA")
+                      os.system("cls")
                       break
                          
             else:
@@ -65,6 +73,7 @@ while menu:
         contrasena1 = input("Ingrese su clave\n")
         opc = int(input("Desea agregar mas usuarios?\n 1.SI\n 2.NO\n"))
         time.sleep(2)
+        os.system("cls")
         if opc == 1:
             usuario2 = input("Ingrese usuario\n")
             contrasena2 = input("Ingrese su clave\n")
